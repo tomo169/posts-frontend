bc2ca44d71b3df5a7f10b1c3e5b62035b55fc886
Tomislav <mar.tomo123@gmail.com>
2022-05-04 17:04:07 +0200

    page for creating data

---
+++ src/views/CreatePostView.vue
@@ -0,0 +1,15 @@
+<template>
+  <FormInputs />
+</template>
+
+<script>
+import FormInputs from '@/components/FormInputs.vue';
+export default {
+  name: 'CreatePostView',
+  components: {
+    FormInputs,
+  },
+};
+</script>
+
+<style></style>
